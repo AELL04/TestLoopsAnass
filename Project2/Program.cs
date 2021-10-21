bde729eb2e7eccc77db07ffbb4e1e95944c2c63d
anass <anass@LAPTOP-NIR3TOA5>
2021-10-21 14:13:46 +0200

    Add project files.

---
+++ Project2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string input = "S";
+            double markE = 0;
+            double markP = 0;
+            double markN = 0;
+            double sum = 0;
+            double gemiddelde = 0;
+
+            do
+            {
+                Console.WriteLine("Geef de naam van de student in");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Geef punten voor Engels");
+                markE = double.Parse(Console.ReadLine());
+                Console.WriteLine("Geef punten voor Programatie");
+                markP = double.Parse(Console.ReadLine());
+                Console.WriteLine("Geef punten voor Netwerken");
+                markN = double.Parse(Console.ReadLine());
+
+                if (true)
+                {
+                    sum = markE + markP + markN;
+                    gemiddelde = sum / 3;
+                }
+                break;
+                
+            } while (input == "S");
+            Console.WriteLine($"{name} heeft in totaal {sum} punten gehaald, waarvan het gemiddelde {gemiddelde} is");
+        }
+    }
+}
---
+++ TestLoopsAnass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLoopsAnass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grootsteGetal = 0;
+            int getal = 0;
+            int i = 0;
+
+            while (i <= 2)
+            {
+                Console.WriteLine("Geef een getal in");
+                getal = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    grootsteGetal = getal;
+                }
+                if (getal > grootsteGetal)
+                {
+                    grootsteGetal = getal;
+                }
+                i++;
+            }
+            Console.WriteLine($"Het grootste getal is {grootsteGetal}");
+        }
+    }
+}
